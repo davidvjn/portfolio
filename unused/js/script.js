a464ee7201b9b0d70f6d03853f2e5d13c81f1802
davidvjn <49305618+davidvjn@users.noreply.github.com>
2020-07-28 13:07:24 +0200

    push push

--- unused/js/script.js
+++ unused/js/script.js
@@ -1,7 +1,7 @@
 // Animation ProgressBar
 function modifValues() {
     var val = $('#progress progress').attr('value');
-    if (val >= 100) { val = 5; }
+    if (val >= 100) { val = 10; }
     var newVal = val * 1 + 0.25;
     var txt = Math.floor(newVal) + '%';
 
